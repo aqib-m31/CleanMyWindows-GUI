96082b8ea1d5e523963f4ad3166925afb252ba5c
Aqib Javid Bhat <aqib.m31.dev@gmail.com>
2023-08-08 15:56:12 +0530

    Modify run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from os import system
 
 
 def main():
+    # Install requirements if arguments are passed (Running for first time)
     parser = argparse.ArgumentParser(
         prog="Clean My Windows", description="Cleans junk files."
     )
@@ -21,7 +22,7 @@ def main():
 def install_requirements():
     with open("requirements.txt") as file:
         for pkg in file.readlines():
-            system(f"pip install {pkg.strip()}")
+            system(f"pip install -q {pkg.strip()}")
 
 
 if __name__ == "__main__":
