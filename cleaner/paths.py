900d86a5015533c1d1279547d10b615b1ad89b53
Aqib Javid Bhat <aqib.m31.dev@gmail.com>
2023-08-11 09:55:24 +0530

    Clean all files

--- cleaner/paths.py
+++
@@ -1,7 +0,0 @@
-from os import path
-
-
-USER_TEMP_DIR = path.expanduser(r"~\AppData\Local\Temp")
-SYSTEM_TEMP_DIR = r"C:\Windows\Temp"
-PREFETCH_DIR = r"C:\Windows\Prefetch"
-LOCAL_DIR = path.expanduser(r"~\AppData\Local")
